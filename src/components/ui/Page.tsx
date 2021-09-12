b54159f32f31017a3ff3856e023fa873ecf41550
Levente Mathe <levente.l.mathe@gmail.com>
2021-09-12 13:19:49 +0200

    add start page

--- src/components/ui/Page.tsx
+++ src/components/ui/Page.tsx
@@ -2,17 +2,11 @@ import { styled } from '@material-ui/core/styles';
 import { Box } from '@material-ui/core';
 import { ChildrenProps } from 'types';
 
-const PageStyles = styled(Box)(({ theme: { breakpoints } }) => ({
-  [breakpoints.down('sm')]: {
-    padding: '1rem',
-  },
-  [breakpoints.up('md')]: {
-    padding: '20%',
-  },
-  [breakpoints.up('lg')]: {
-    width: '33%',
-  },
-}));
+const PageStyles = styled(Box)({
+  display: 'grid',
+  placeItems: 'center',
+  height: '100vh',
+});
 
 export const Page = ({ children }: ChildrenProps) => (
   <PageStyles>{children}</PageStyles>
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,3 +1,42 @@
 import { Page } from 'components/ui';
+import { styled } from '@material-ui/core/styles';
+import { Box, Button, Typography } from '@material-ui/core';
+import { useGameState } from 'state/game';
 
-export const Start = () => <Page>START</Page>;
+const Content = styled(Box)({
+  '&>*': {
+    marginBottom: '1rem',
+  },
+});
+
+const StartButton = styled(Button)({
+  margin: '0 auto',
+  display: 'block',
+});
+
+export const Start = () => {
+  const { send } = useGameState();
+
+  return (
+    <Page>
+      <Content>
+        <Typography align="center" variant="h3">
+          Welcome to the Trivia Challenge!
+        </Typography>
+        <Typography align="center" variant="body1">
+          You will be presented with 10 True or False questions.
+        </Typography>
+        <Typography align="center" variant="body1">
+          Can you score a 100%?
+        </Typography>
+        <StartButton
+          variant="contained"
+          color="primary"
+          onClick={() => send('START')}
+        >
+          BEGIN
+        </StartButton>
+      </Content>
+    </Page>
+  );
+};
