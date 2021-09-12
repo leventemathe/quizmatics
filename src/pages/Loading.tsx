4fc49addabf18a8a72b4d2cd496d9bb8d06f8a67
Levente Mathe <levente.l.mathe@gmail.com>
2021-09-12 19:23:16 +0200

    add spinner to loading page

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,3 +1,8 @@
+import { CircularProgress } from '@material-ui/core';
 import { Page } from 'components/ui';
 
-export const Loading = () => <Page>Loading</Page>;
+export const Loading = () => (
+  <Page>
+    <CircularProgress />
+  </Page>
+);
