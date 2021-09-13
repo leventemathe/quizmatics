ee4254cc07ed067fda076297499766d7d80cca31
Levente Mathe <levente.l.mathe@gmail.com>
2021-09-13 16:12:55 +0200

    add state machine tests

--- src/state/game/gameStateMachine.test.ts
+++ src/state/game/gameStateMachine.test.ts
@@ -1,55 +1,139 @@
 import { interpret } from 'xstate';
 import { request } from 'networking';
+import { gameConfig } from 'config/gameConfig';
+import { generateFakeQuestionApiResponse } from 'test-utils/fakeApi';
 import { faker } from 'test-utils';
 import { gameStateMachine } from './gameStateMachine';
-import { gameConfig } from '../../config/gameConfig';
 
-let requestMock: jest.SpyInstance;
-
-const getAnswerFromBool = (bool: boolean) => (bool ? 'True' : 'False');
-
-const generateFakeQuestions = () => {
-  const questions = [];
-
-  for (let i = 0; i < gameConfig.amountOfQuestions; i += 1) {
-    const correctAnswer = faker.datatype.boolean();
-
-    questions.push({
-      category: faker.name.title(),
-      type: 'boolean',
-      difficulty: 'hard',
-      question: faker.name.title(),
-      correct_answer: getAnswerFromBool(correctAnswer),
-      incorrect_answers: [getAnswerFromBool(!correctAnswer)],
-    });
-  }
-
-  return questions;
-};
+jest.mock('networking/request');
 
 beforeEach(() => {
-  requestMock = jest.spyOn({ request }, 'request');
-
-  requestMock.mockResolvedValue({
-    response_code: 0,
-    results: generateFakeQuestions(),
-  });
+  (request as jest.Mock).mockResolvedValue(generateFakeQuestionApiResponse());
 });
 
 afterEach(() => {
   jest.resetAllMocks();
 });
 
-test('game starts in idle state', () => {
+test('the game starts in idle state', () => {
   const gameService = interpret(gameStateMachine);
   gameService.start();
 
   expect(gameService.state.matches('idle')).toBe(true);
 });
 
-test('game starts loading on START event', (done) => {
+test('the game starts loading on START event', (done) => {
   const gameService = interpret(gameStateMachine).onTransition((state) => {
     if (state.matches('loading')) done();
+  });
+
+  gameService.start();
+  gameService.send('START');
+});
+
+test('the game correctly handles successful fetch', (done) => {
+  const gameService = interpret(gameStateMachine).onTransition((state) => {
+    if (state.matches('playing')) {
+      expect(request).toHaveBeenCalledTimes(1);
+      expect(state.context.questions.length).toBe(gameConfig.amountOfQuestions);
+      expect(state.context.currentIndex).toBe(0);
+      done();
+    }
+  });
+
+  gameService.start();
+  gameService.send('START');
+});
+
+test('the game correctly handles failing fetch', (done) => {
+  (request as jest.Mock).mockRejectedValue({});
+
+  const gameService = interpret(gameStateMachine).onTransition((state) => {
+    if (state.matches('error')) {
+      expect(request).toHaveBeenCalledTimes(1);
+      expect(state.context.questions.length).toBe(0);
+      expect(state.context.currentIndex).toBe(0);
+      done();
+    }
+  });
+
+  gameService.start();
+  gameService.send('START');
+});
+
+test('the game can restart from the error state', (done) => {
+  (request as jest.Mock)
+    .mockRejectedValueOnce({ error: 'some error' })
+    .mockResolvedValueOnce(generateFakeQuestionApiResponse());
+
+  const gameService = interpret(gameStateMachine).onTransition((state) => {
+    if (state.matches('error')) {
+      gameService.send('RESTART');
+    }
+
+    if (state.matches('playing')) {
+      expect(request).toHaveBeenCalledTimes(2);
+      expect(state.context.questions.length).toBe(gameConfig.amountOfQuestions);
+      expect(state.context.currentIndex).toBe(0);
+      done();
+    }
+  });
+
+  gameService.start();
+  gameService.send('START');
+});
+
+test('can answer a question', (done) => {
+  let answerCount = 0;
+
+  const gameService = interpret(gameStateMachine).onTransition((state) => {
+    if (state.matches('playing')) {
+      if (answerCount === 0) {
+        gameService.send({ type: 'ANSWER', answer: faker.datatype.boolean() });
+      } else {
+        expect(state.context.questions[0].answer).not.toBeUndefined();
+        done();
+      }
+
+      answerCount += 1;
+    }
+  });
+
+  gameService.start();
+  gameService.send('START');
+});
+
+test('game can get to the result state by answering all questions', (done) => {
+  const gameService = interpret(gameStateMachine).onTransition((state) => {
+    if (state.matches('playing')) {
+      gameService.send({ type: 'ANSWER', answer: faker.datatype.boolean() });
+    }
+    if (state.matches('finished')) {
+      state.context.questions.forEach((question) => {
+        expect(question.answer).not.toBeUndefined();
+      });
+      done();
+    }
+  });
+
+  gameService.start();
+  gameService.send('START');
+});
+
+test('game can be restarted in finished state', (done) => {
+  let loadingCount = 0;
+
+  const gameService = interpret(gameStateMachine).onTransition((state) => {
+    if (state.matches('loading')) {
+      loadingCount += 1;
+      if (loadingCount === 2) done();
+    }
+    if (state.matches('playing')) {
+      gameService.send({ type: 'ANSWER', answer: faker.datatype.boolean() });
+    }
+    if (state.matches('finished')) {
+      gameService.send('RESTART');
+    }
   });
 
   gameService.start();
---
+++ src/test-utils/fakeApi.ts
@@ -0,0 +1,28 @@
+import { faker } from 'test-utils';
+import { gameConfig } from 'config/gameConfig';
+
+const getAnswerFromBool = (bool: boolean) => (bool ? 'True' : 'False');
+
+export const generateFakeQuestions = () => {
+  const questions = [];
+
+  for (let i = 0; i < gameConfig.amountOfQuestions; i += 1) {
+    const correctAnswer = faker.datatype.boolean();
+
+    questions.push({
+      category: faker.name.title(),
+      type: 'boolean',
+      difficulty: 'hard',
+      question: faker.name.title(),
+      correct_answer: getAnswerFromBool(correctAnswer),
+      incorrect_answers: [getAnswerFromBool(!correctAnswer)],
+    });
+  }
+
+  return questions;
+};
+
+export const generateFakeQuestionApiResponse = () => ({
+  response_code: 0,
+  results: generateFakeQuestions(),
+});
