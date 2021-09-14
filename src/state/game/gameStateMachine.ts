9f6e284b6fe031ef8df994d0c2649bec2299dd1e
Levente Mathe <levente.l.mathe@gmail.com>
2021-09-14 15:25:21 +0200

    refactor actions

--- src/state/game/gameStateMachine.ts
+++ src/state/game/gameStateMachine.ts
@@ -14,6 +14,41 @@ export type GameEvent =
   | { type: 'ANSWER'; answer: boolean }
   | { type: 'RESTART' };
 
+const resetCurrentIndex = assign<GameContext, GameEvent>({
+  currentIndex: 0,
+});
+
+const saveQuestions = assign<GameContext, GameEvent>({
+  // There's no nicer way than to use any: https://xstate.js.org/docs/guides/typescript.html
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  questions: (_context, event: any) => event.data,
+});
+
+const saveError = assign<GameContext, GameEvent>({
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  error: (_context, event: any) => event.data,
+});
+
+const saveAnswer = assign<GameContext, GameEvent>({
+  questions: (context, event) => {
+    if (event.type !== 'ANSWER') return context.questions;
+
+    const { questions, currentIndex } = context;
+    const newQuestions = [...questions];
+    newQuestions[currentIndex].answer = event.answer;
+
+    return newQuestions;
+  },
+  currentIndex: (context) => context.currentIndex + 1,
+});
+
+const canFinishGame = (context: GameContext) => {
+  const asnweredQuestions = context.questions.filter(
+    (question) => question.answer !== undefined
+  );
+  return asnweredQuestions.length === gameConfig.amountOfQuestions;
+};
+
 export const gameStateMachine = createMachine<GameContext, GameEvent>(
   {
     id: 'game',
@@ -66,36 +101,13 @@ export const gameStateMachine = createMachine<GameContext, GameEvent>(
   },
   {
     actions: {
-      resetCurrentIndex: assign<GameContext, GameEvent>({
-        currentIndex: 0,
-      }),
-      saveQuestions: assign({
-        // There's no nicer way than to use any: https://xstate.js.org/docs/guides/typescript.html
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        questions: (_context, event: any) => event.data,
-      }),
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      saveError: assign({ error: (_context, event: any) => event.data }),
-      saveAnswer: assign<GameContext, GameEvent>({
-        questions: (context, event) => {
-          if (event.type !== 'ANSWER') return context.questions;
-
-          const { questions, currentIndex } = context;
-          const newQuestions = [...questions];
-          newQuestions[currentIndex].answer = event.answer;
-
-          return newQuestions;
-        },
-        currentIndex: (context) => context.currentIndex + 1,
-      }),
+      resetCurrentIndex,
+      saveQuestions,
+      saveError,
+      saveAnswer,
     },
     guards: {
-      canFinishGame: (context) => {
-        const asnweredQuestions = context.questions.filter(
-          (question) => question.answer !== undefined
-        );
-        return asnweredQuestions.length === gameConfig.amountOfQuestions;
-      },
+      canFinishGame,
     },
   }
 );
